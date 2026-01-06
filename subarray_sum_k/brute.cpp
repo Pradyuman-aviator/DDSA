32b039bd993320bb4c42ea6a6b8393b475c21966
its-me-meax <ultragaming123q@gmail.com>
2026-01-07 00:00:24 +0530

    sum=k

---
+++ subarray_sum_k/brute.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+               int n = nums.size();
+        int count = 0;
+
+     
+        for (int i = 0; i < n; i++) {
+
+          
+            for (int j = i; j < n; j++) {
+
+                int sum = 0;
+
+                for (int t = i; t <= j; t++) {
+                    sum += nums[t];
+                }
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+};
