4c3f314d30f5165674f9bfc7fe21a4a147a241eb
its-me-meax <ultragaming123q@gmail.com>
2026-01-13 21:21:37 +0530

    corrected brute

--- merge_intervals/brute.cpp
+++ merge_intervals/brute.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<int> used(n, 0);
-        vector<vector<int>> ans;
+    int n =intervals.size();
+    vector<vector<int>> ans;
+    sort(intervals.begin(),intervals.end());
+    for(int i=0;i<n;i++){
+        int start= intervals[i][0];
+        int end = intervals[i][1];
+        if(!ans.empty() && end<= ans.back()[1] ){
+            continue;
 
-        for (int i = 0; i < n; i++) {
-            if (used[i]) continue;
-            int start = intervals[i][0];
-            int end   = intervals[i][1];
-            used[i] = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (used[j]) continue;
-                int a = intervals[j][0];
-                int b = intervals[j][1];
-               
-                if (a <= end && b >= start) {
-                    start = min(start, a);
-                    end   = max(end, b);
-                    used[j] = 1;
-                }
-            }
-
-            ans.push_back({start, end});
         }
-        return ans;
+        for(int j=i+1;j<n;j++){
+            if(intervals[j][0]<=end){
+                end=max(end,intervals[j][1]);
+            }
+            else {
+                break;
+            }
+        }
+ans.push_back({start,end});
+    }
+    return ans;
+
     }
 };
