130a5dd3e82186ba0373c0c69e5a08afa34e88c0
its-me-meax <ultragaming123q@gmail.com>
2026-01-18 10:07:32 +0530

    done

---
+++ count_inversions/optimal.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int cnt = 0;
+
+void merge(vector<int>& arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left++]);
+        } 
+        else {
+            cnt += (mid-left+1);
+            temp.push_back(arr[right++]);
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left++]);
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right++]);
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int>& arr, int low, int high) {
+    if (low >= high) return;
+
+    int mid = low + (high - low) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+   long long int numberOfInversions(vector<int> nums) {
+    int n =nums.size();
+    mergeSort(nums,0,n-1);
+
+    return cnt;
+
+    }
+};
