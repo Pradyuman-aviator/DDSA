a70e6b4b14c7531c808aafc05d1c71d93d51b3c2
its-me-meax <ultragaming123q@gmail.com>
2026-01-23 12:35:18 +0530

    done

--- binary search/first_and_last_pos/logn.cpp
+++ binary search/first_and_last_pos/logn.cpp
@@ -30,3 +30,48 @@ public:
         return ans;
     }
 };
+
+
+///////////
+//O(log n) approach
+class Solution {
+public:
+    int lowerBound(vector<int> &nums, int x) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int upperbound(vector<int> &nums, int x) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lb = lowerBound(nums, target);
+        if (lb == n || nums[lb] != target) return {-1, -1};
+        return {lb, upperbound(nums, target) - 1};
+    }
+};
