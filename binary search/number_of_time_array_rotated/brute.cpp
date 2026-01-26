188c38460c9a4ecabb24f5ded1db846887d73557
its-me-meax <ultragaming123q@gmail.com>
2026-01-26 16:18:56 +0530

    done

---
+++ binary search/number_of_time_array_rotated/brute.cpp
@@ -0,0 +1,8 @@
+int countRotations(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] > arr[i + 1])
+            return i + 1;
+    }
+    return 0;
+}
