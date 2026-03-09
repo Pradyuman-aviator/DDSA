aa553171e3b37ce7c5ebe6dd6662015e5952a377
its-me-meax <ultragaming123q@gmail.com>
2026-03-09 09:42:23 +0530

    done

--- binary search/splitarray/binary_search.cpp
+++ binary search/splitarray/binary_search.cpp
@@ -4,36 +4,36 @@ using namespace std;
 class Solution {
 public:
 
-    bool possible(vector<int>& nums, int students, int limit){
-        int count = 1;
-        int pages = 0;
+    bool possible(vector<int>& nums, int k, int limit){
+        int parts = 1;
+        int sum = 0;
 
         for(int i = 0; i < nums.size(); i++){
-            if(pages + nums[i] <= limit){
-                pages += nums[i];
+
+            if(sum + nums[i] <= limit){
+                sum += nums[i];
             }
             else{
-                count++;
-                pages = nums[i];
+                parts++;
+                sum = nums[i];
             }
         }
 
-        return count <= students;
+        return parts <= k;
     }
 
-    int findPages(vector<int> &nums, int m)  {
-
-        int n = nums.size();
-        if(m > n) return -1;
+    int splitArray(vector<int>& nums, int k) {
 
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
+
         int ans = -1;
 
         while(low <= high){
+
             int mid = (low + high) / 2;
 
-            if(possible(nums, m, mid)){
+            if(possible(nums, k, mid)){
                 ans = mid;
                 high = mid - 1;
             }
