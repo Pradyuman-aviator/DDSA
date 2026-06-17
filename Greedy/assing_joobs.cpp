bcfb92ad22f69d21eebb9ebda770caeaa39765f3
its-me-meax <ultragaming123q@gmail.com>
2026-06-17 10:12:01 +0530

    done

--- Greedy/assing_joobs.cpp
+++ Greedy/assing_joobs.cpp
@@ -51,3 +51,59 @@ class Solution {
 */ 
 
 /// i was sorting the map lol
+
+
+/// with the pair container
+
+
+class Solution {
+public:
+    vector<int> jobSequencing(vector<int> &deadline,
+                              vector<int> &profit) {
+
+        int n = deadline.size();
+
+        int num_jobs_done = 0;
+        int total_profit = 0;
+
+        vector<pair<int,int>> Jobs;
+
+        for(int i = 0; i < n; i++) {
+            Jobs.push_back({deadline[i], profit[i]});
+        }
+
+        sort(Jobs.begin(), Jobs.end(),
+             [](auto &a, auto &b) {
+                 return a.second > b.second;
+             });
+
+        int mxDeadline = 0;
+
+        for(int d : deadline) {
+            mxDeadline = max(mxDeadline, d);
+        }
+
+        vector<int> task(mxDeadline + 1, 0);
+
+        for(auto &Job : Jobs) {
+
+            int d = Job.first;
+            int p = Job.second;
+
+            for(int i = d; i > 0; i--) {
+
+                if(!task[i]) {
+
+                    task[i] = 1;
+
+                    num_jobs_done++;
+                    total_profit += p;
+
+                    break;
+                }
+            }
+        }
+
+        return {num_jobs_done, total_profit};
+    }
+};
