55edf85aabd70be2c143a4fbc4a48780fe1a1434
its-me-meax <ultragaming123q@gmail.com>
2026-07-25 20:04:30 +0530

    done

---
+++ Binary_search_coderarmy/Koko_eating_bananas.cpp
@@ -0,0 +1,23 @@
+// it is the most optimal one hell yeah
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start = 1;
+        int end = *max_element(piles.begin(), piles.end());
+        int ans = end;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long long hours = 0;
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+            if (hours <= h) {
+                ans = mid;
+                end = mid - 1;      
+            } else {
+                start = mid + 1;    
+            }
+        }
+        return ans;
+    }
+};
