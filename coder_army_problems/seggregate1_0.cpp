523e4128fd31ecccf4510ed6a7563666cfdd86d1
its-me-meax <ultragaming123q@gmail.com>
2026-07-26 10:12:05 +0530

    done

---
+++ coder_army_problems/seggregate1_0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        int n = arr.size();
+        
+        int count0= 0;
+        int count1=0;
+        
+        
+        for(int i = 0 ;i < n ;i++){
+            if(arr[i] ==0) count0++;
+            if(arr[i]==1) count1++;
+        }
+        arr.erase(arr.begin(), arr.end());
+        
+        int zero = 0;
+        int one =1;
+        
+        
+        for(int i = 0;i<count0;i++){
+            arr.push_back(zero);
+        }
+        for(int i= count0;i<n;i++){
+            arr.push_back(one);
+        }
+    }
+};
