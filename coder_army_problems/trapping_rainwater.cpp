d99b1bc76cfd57757c470de800902d23fec8fae1
its-me-meax <ultragaming123q@gmail.com>
2026-08-03 11:23:16 +0530

    done

---
+++ coder_army_problems/trapping_rainwater.cpp
@@ -0,0 +1,33 @@
+// brute force TC - >  O(N) & SC-> O(N)
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        vector<int> leftmax(n) , rightmax(n);
+
+        //storing the left max into the array
+        leftmax[0]=height[0];
+
+        for(int i=1;i<n;i++){
+            leftmax[i] = max(leftmax[i-1],height[i]);
+
+        }
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i] = max(rightmax[i+1],height[i]);
+
+        }
+
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            ans +=  min(leftmax[i],rightmax[i])-height[i];
+
+
+        }
+        return ans;
+    }
+};
