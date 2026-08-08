add385178089042c1acbe04d5805e751a2564e76
its-me-meax <ultragaming123q@gmail.com>
2026-08-08 11:48:32 +0530

    done

--- coder_army_problems/3_sum.cpp
+++ coder_army_problems/3_sum.cpp
@@ -45,3 +45,66 @@ public:
         
     }
 };
+
+
+
+/// written by meee soltuuion and all 
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+       vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+
+
+        // skiping the duplicate elemnts at the first 
+
+        
+        
+
+        for(int i = 0;i<n-2;i++){
+
+            if(i>0 && nums[i] == nums[i-1]){
+            continue;
+        }
+            int a = -nums[i];
+            int start = i+1;
+            int end = n-1;
+
+            while(start<end){
+
+                if((nums[start]+nums[end]) == a){
+               ans.push_back({nums[i],nums[start],nums[end]});
+                start++;
+                end--;
+
+
+                // checkin for the duplicates
+
+                while(start<end && nums[start]==nums[start-1]){
+                    start++;
+                }
+                while(start<end && nums[end]==nums[end+1]){
+                    end--;
+                }
+                
+
+                }
+                else if((nums[start] + nums[end]) > a){
+                    end--;
+                }
+                else{
+                    start++;
+                }
+
+            }
+
+            
+        }
+        return ans;
+
+        
+    }
+};
