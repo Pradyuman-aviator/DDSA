b5ba43047540ed3b2b6ff8d42bfdf1bd61dd0739
its-me-meax <ultragaming123q@gmail.com>
2026-08-11 13:34:57 +0530

    done

---
+++ coder_army_problems/2-D array/find_row_with_max_sum_and return_index.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int arr1[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int maxSum = INT_MIN;
+    int max_index = -1;
+
+    for(int i = 0; i < 3; i++) {
+
+        int Sum = 0;   // reset for every row
+
+        for(int j = 0; j < 4; j++) {
+            Sum += arr1[i][j];
+        }
+
+        if(Sum > maxSum) {
+            maxSum = Sum;
+            max_index = i;
+        }
+    }
+
+    cout << max_index;
+
+    return 0;
+}
