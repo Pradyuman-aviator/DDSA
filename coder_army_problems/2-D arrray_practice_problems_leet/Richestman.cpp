bc8efc64a20a4d25295cf8429e17d65c3084f6ff
its-me-meax <ultragaming123q@gmail.com>
2026-08-12 12:08:09 +0530

    done

--- coder_army_problems/2-D arrray_practice_problems_leet/Richestman.cpp
+++ coder_army_problems/2-D arrray_practice_problems_leet/Richestman.cpp
@@ -24,3 +24,28 @@ return Richest_guy;
         
     }
 };
+
+
+/// next approach ( STL)
+
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int rows = accounts.size();
+
+        int columns = accounts[0].size();
+
+        int ans = INT_MIN;
+
+        for(int i=0;i<rows;i++){
+            // accumulate function of CPP STL
+            int sum =  accumulate(accounts[i].begin(),accounts[i].end(),0);
+
+            ans = max(ans,sum);
+
+        }
+
+        return ans;
+    }
+};
