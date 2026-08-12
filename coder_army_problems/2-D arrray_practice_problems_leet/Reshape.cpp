e4994b60c6f7ca693228cf317762a03a6717cd68
its-me-meax <ultragaming123q@gmail.com>
2026-08-12 15:44:37 +0530

    done

--- coder_army_problems/2-D arrray_practice_problems_leet/Reshape.cpp
+++ coder_army_problems/2-D arrray_practice_problems_leet/Reshape.cpp
@@ -22,11 +22,47 @@ public:
         }
 
         return ans;
-
-
-   
-
  
+        
+    }
+};
+
+
+/// alternative slution 
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int rows = mat.size();
+
+        int columns = mat[0].size();
+
+        if(r*c != rows*columns){
+            return mat;
+            
+        }
+
+        vector<vector<int>> ans(r,vector<int>(c));
+
+
+
+        int newrow = 0;
+        int newcol = 0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                ans[newrow][newcol] = mat[i][j];
+
+                newcol++;
+            if(newcol==c){
+                newcol=0;
+                newrow++;
+
+            }
+            }
+        }
+
+        return ans;
         
     }
 };
