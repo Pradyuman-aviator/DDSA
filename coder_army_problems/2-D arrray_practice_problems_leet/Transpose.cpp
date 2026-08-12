ec8427133c056a4b3a5ff3a39ff796aec179a8a3
its-me-meax <ultragaming123q@gmail.com>
2026-08-12 13:04:56 +0530

    done

--- coder_army_problems/2-D arrray_practice_problems_leet/Transpose.cpp
+++ coder_army_problems/2-D arrray_practice_problems_leet/Transpose.cpp
@@ -20,3 +20,26 @@ public:
         return ans;
     }
 };
+
+
+
+///but if we have square matrix then we can do this lolll 
+
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                swap(matrix[i][j], matrix[j][i]);
+
+            }
+        }
+
+        return matrix;
+    }
+};
