0029911b949a4b96cc6591e729dbfdce4eae91e7
its-me-meax <ultragaming123q@gmail.com>
2026-08-13 15:26:33 +0530

    done

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -11,16 +11,11 @@ class Solution {
     q.push(0);
     
     while(!q.empty()){
-        int node = q.front();   
-        
-        q.pop();
-        
-        ans.push_back(node);
-        
-        for(int j=0;j<adj[node].size();j++){
-            
-            if(!visited[adj[node][j]]){
-                
+        int node = q.front();     
+        q.pop();   
+        ans.push_back(node); 
+        for( int j=0;j<adj[node].size();j++){   
+            if(!visited[adj[node][j]]){      
                 visited[adj[node][j]]=1;
                 q.push(adj[node][j]);
             }
