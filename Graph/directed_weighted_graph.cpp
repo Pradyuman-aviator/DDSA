dbac0a2872497d8a830c00ebb06680ad8fcfab76
its-me-meax <ultragaming123q@gmail.com>
2026-08-13 08:48:40 +0530

    done

---
+++ Graph/directed_weighted_graph.cpp
@@ -0,0 +1,19 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int edges,vertex;
+
+    vector<vector<int>> ans(r,vector<int>(c,0));
+
+    }
+
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++)
+{
+    cout << adjMat[i][j]<< " ";
+}
+    }
+    cout << endl;
+
+}
