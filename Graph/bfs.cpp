3b05cefcc1db755509ca191b650382091affb52b
its-me-meax <ultragaming123q@gmail.com>
2026-08-13 15:24:21 +0530

    done

---
+++ Graph/bfs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+         queue<int> q;
+         int V = adj.size();
+    
+    vector<int> ans;
+    vector<bool> visited(V,0);
+    
+    visited[0]=1;
+    q.push(0);
+    
+    while(!q.empty()){
+        int node = q.front();   
+        
+        q.pop();
+        
+        ans.push_back(node);
+        
+        for(int j=0;j<adj[node].size();j++){
+            
+            if(!visited[adj[node][j]]){
+                
+                visited[adj[node][j]]=1;
+                q.push(adj[node][j]);
+            }
+        }
+    }
+    
+    return ans;
+        
+        
+    }
+};
