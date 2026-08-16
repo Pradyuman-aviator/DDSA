29f142670a4dfd10b3cf054d3898f2663ac1c2b0
its-me-meax <ultragaming123q@gmail.com>
2026-08-16 23:06:22 +0530

    done

---
+++ coder_army_problems/warmer_day.cpp
@@ -0,0 +1,35 @@
+// brute force O(n2)
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        int n = temperatures.size();
+
+        vector<int> ans;
+
+        for(int i = 0;i<n;i++) {
+            int today_temp = temperatures[i];
+            int days = 0;
+            for(int j =i+1;j<n;j++) {
+                days++;
+                int current_temp =temperatures[j];
+
+                if(current_temp> today_temp) {
+                    ans.push_back(days);
+                    break;
+                }
+                if(j==n-1) {
+                    ans.push_back(0);
+                }
+            }
+            // Handles the last element
+            if(i == n - 1) {
+                ans.push_back(0);
+            }
+        }
+
+        return ans;
+    }
+};
