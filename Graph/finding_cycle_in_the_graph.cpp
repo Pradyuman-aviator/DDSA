a2fbe5b09dc95b1745dd8a2bd6f587cd8a4ea9a2
its-me-meax <ultragaming123q@gmail.com>
2026-08-17 09:37:23 +0530

    done

---
+++ Graph/finding_cycle_in_the_graph.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    bool DetectCycle(int node, int parent,
+                     vector<vector<int>>& adj,
+                     vector<int>& Visited) {
+
+        Visited[node] = 1;
+
+        for(int neighbour : adj[node]) {
+
+            if(neighbour == parent) {
+                continue;
+            }
+
+            if(Visited[neighbour]) {
+                return true;
+            }
+
+            if(DetectCycle(neighbour, node, adj, Visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adj(V);
+
+        // Convert edge list -> adjacency list
+        for(auto edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> Visited(V, 0);
+
+        for(int i = 0; i < V; i++) {
+
+            if(!Visited[i]) {
+
+                if(DetectCycle(i, -1, adj, Visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
