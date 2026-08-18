e04c7df3fb1ab6ec50e7502dbd1a03e9a3cd1ee1
its-me-meax <ultragaming123q@gmail.com>
2026-08-18 22:29:25 +0530

    done

--- Graph/topological_sort.cpp
+++ Graph/topological_sort.cpp
@@ -58,3 +58,75 @@ class Solution {
         
     }
 };
+
+
+
+
+//// by using BFS 
+
+
+class Solution {
+  public:
+  
+  
+    
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+    
+        
+        /// conversion edge list -> ajoint list
+        
+        for(auto edge : edges){
+            
+            int u = edge[0];
+            int v  = edge[1];
+            
+            adj[u].push_back(v);
+                
+        }
+        
+        vector<int> InDeg(V,0);
+        vector<int> ans;
+        
+        for(int i =0;i<V;i++){
+            for(int j=0;j<adj[i].size();j++){
+            
+            
+            InDeg[adj[i][j]]++;
+        }
+        
+        }
+        
+        
+        queue<int> q;
+        
+        
+        for(int i =0;i<V;i++){
+            if(!InDeg[i]){
+                q.push(i);
+            }
+            
+        }
+        
+        while(!q.empty()){
+            
+        int node = q.front();
+        
+        
+        q.pop();
+        ans.push_back(node);
+        
+        
+        for(int i =0;i<adj[node].size();i++){
+            InDeg[adj[node][i]]--;
+            if(!InDeg[adj[node][i]]){
+                q.push(adj[node][i]);
+            }
+        }
+            
+            
+        }
+        
+       return ans;
+    }
+};
