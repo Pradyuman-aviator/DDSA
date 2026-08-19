a52a500063c7d45ba6d9a36bc94bc54ce8fc5d66
its-me-meax <ultragaming123q@gmail.com>
2026-08-19 22:11:23 +0530

    done

--- Stack/implementation_of_stack_using_linked_list.cpp
+++ Stack/implementation_of_stack_using_linked_list.cpp
@@ -1,19 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Node
-{
-public:
-    int data;
-    Node* next;
-
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
-
 class Stack
 {
     Node* top;
@@ -27,40 +11,51 @@ public:
 
     void push(int val)
     {
-
         Node* temp = new Node(val);
 
         temp->next = top;
         top = temp;
     }
 
-   void pop()
-{
-    if(top == nullptr)
+    void pop()
     {
-        cout << "Underflow";
-        return;
+        if(top == nullptr)
+        {
+            cout << "Underflow";
+            return;
+        }
+
+        Node* temp = top;
+        top = top->next;
+        delete temp;
     }
 
+    int peek()
+    {
+        if(top == nullptr)
+        {
+            return -1;
+        }
+
+        return top->data;
+    }
+
+    bool is_empty()
+    {
+        return top == nullptr;
+    }
+
+    int getSize()
+{
+    int count = 0;
     Node* temp = top;
-    top = top->next;
-    delete temp;
-}
 
-   int peek()
-{
-    if(top == nullptr)
+    while(temp != nullptr)
     {
-        return -1;
+        count++;
+        temp = temp->next;
     }
 
-    return top->data;
+    return count;
 }
-
-bool is_empty(){
-    return top==nullptr;
-}
-
-
-
 };
