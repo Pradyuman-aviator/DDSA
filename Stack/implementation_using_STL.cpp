41963c440c4dc553d111e9e8ce46c84cf0f0b28b
its-me-meax <ultragaming123q@gmail.com>
2026-08-20 08:46:00 +0530

    done

---
+++ Stack/implementation_using_STL.cpp
@@ -0,0 +1,17 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    ///defining a stack
+
+    stack<int> s;
+
+    s.push(5);
+    s.push(5);
+    s.push(4);
+
+    s.empty();
+    
+
+
+}
