2ac4378cf158bb918918efded8d92ad9049ba7d7
its-me-meax <ultragaming123q@gmail.com>
2026-08-20 13:40:11 +0530

    done

--- Stack/reverse_array_using_stack.cpp
+++ Stack/reverse_array_using_stack.cpp
@@ -10,11 +10,31 @@ class Solution {
      }
      
      /// adding them back to the array
-     
+
    for(int i = 0; i < arr.size(); i++) {
        arr[i] = s.top();
        s.pop();
-   }
-        
+       }    }
+};
+
+///r eversirng hte string using ths staxkx
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+    stack<int> st;
+        for(auto a : s){
+            st.push(a);
+
+        }
+        int i =0;
+        while(!st.empty()){
+            s[i] = st.top();
+            i++;
+            st.pop();            
+        }
     }
 };
+
